7e3240cb65ad7e79c61faf16b2357d5f8aae90c2
David Guan <davidguan@realtek.com>
2012-12-19 14:05:26 +0800

    Add add function

--- foo.c
+++ foo.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int add_foo(int x, int y){
+
+   return (x + y);
+}
+
 int main(int argc, char *argv[]){
-    printf("Hello! World.\n");
+
+    printf("Hello! World, 2 + 3 is %d.\n", add_foo(2,3));
+
     return 0;
 }
 
