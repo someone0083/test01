bdcf4bf07b991a643723f93f7846a3d74fcb0f3f
David Guan <davidguan@realtek.com>
2012-12-19 14:10:17 +0800

    Add sub function

--- foo.c
+++ foo.c
@@ -5,6 +5,11 @@ int add_foo(int x, int y){
    return (x + y);
 }
 
+int sub_foo(int x, int y){
+
+   return (x - y);
+}
+
 int main(int argc, char *argv[]){
 
     printf("Hello! World, 2 + 3 is %d.\n", add_foo(2,3));
